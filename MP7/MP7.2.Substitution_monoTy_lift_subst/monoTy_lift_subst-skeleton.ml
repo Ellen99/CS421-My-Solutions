ff11f4c2bfaaa9c7a85bd4ac4bc6cb7190068b6f
Elen Chatikyan <echatikyan@gmail.com>
2023-12-04 22:06:18 -0600

    Add files via upload

---
+++ MP7/MP7.2.Substitution_monoTy_lift_subst/monoTy_lift_subst-skeleton.ml
@@ -0,0 +1,5 @@
+open Common
+open Plsolution
+
+let rec monoTy_lift_subst sigma ty =
+  raise (Failure "Not implemented yet.") (* Remove this *)
---
+++ MP7/MP7.2.Substitution_monoTy_lift_subst/student.ml
@@ -0,0 +1,13 @@
+open Common
+open Plsolution
+
+let rec monoTy_lift_subst (sigma: (typeVar * monoTy) list) (ty: monoTy) =
+  match ty with
+|TyVar(t) -> subst_fun sigma t
+|TyConst(str,tvars)-> let resultargs =     
+                        let rec liftvars vars = 
+                            match vars with
+                            |[]->[]
+                            |(v::rest) -> (monoTy_lift_subst sigma v) :: liftvars rest
+                            in liftvars tvars
+                      in TyConst(str, resultargs)
